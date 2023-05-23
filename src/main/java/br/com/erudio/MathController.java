23fa22e0f21539f28d0db8ecf3fed475b2d63fd8
VinySales <vinysalesreboucas@hotmail.com>
2023-05-23 00:31:16 -0300

    RequestMapping com PathVariable

--- src/main/java/br/com/erudio/MathController.java
+++ src/main/java/br/com/erudio/MathController.java
@@ -2,19 +2,22 @@ package br.com.erudio;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+public class MathController {
 	
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 	
-	@RequestMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));	
+	@RequestMapping("/sum/{n1}/{n2}")
+	public Double sum(
+			@PathVariable(value = "n1") String n1,
+			@PathVariable(value = "n2") String n2) {
+		return Double.parseDouble(n1) + Double.parseDouble(n2);
 	}
 	
 	// http://localhost:8080/areaQuadrado?lado=2
@@ -26,6 +29,6 @@ public class GreetingController {
 	// http://localhost:8080/areaRetangulo?lado1=2&lado2=3
 	@RequestMapping("/areaRetangulo")
 	public Integer areaRetangulo(@RequestParam(value = "lado1") Integer lado1, @RequestParam(value = "lado2") Integer lado2) {
-		return lado1 * lado2;	
+		return lado1 * lado2;
 	}
 }
